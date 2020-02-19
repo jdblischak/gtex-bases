bf83903bde8bc5b04c12d05c4a0e2a2d27a3646a
John Blischak <jdblischak@gmail.com>
2020-02-19 15:38:13 -0600

    Add exon boundaries to diagnostic plots.

--- plot-counting-results.R
+++ plot-counting-results.R
@@ -16,6 +16,7 @@ exons <- data.table(
   strand = as.character(strand(gr)),
   name = mcols(gr)[, "gene_name"]
 )
+exons[, gene := stringr::str_replace(gene, "(ENSG\\d+)\\.\\d+", "\\1")]
 setkey(exons, gene)
 
 dir_counts <- "data/counts/"
@@ -26,14 +27,24 @@ files <- list.files(path = dir_counts, pattern = "txt$", full.names = TRUE)
 for (f in files) {
   print(f)
   counts <- fread(f)
+  gene_id <- unique(counts$GeneID)
+
   plotfile <- basename(f)
   plotfile <- tools::file_path_sans_ext(plotfile)
   plotfile <- paste0(plotfile, ".png")
   plotfile <- file.path(dir_plots, plotfile)
+
   totals <- counts[, list(total = base::sum(.SD)),
                    .SDcols = grep("bam$", colnames(counts), value = TRUE),
                    by = Start]
+
+  gene_exons <- exons[gene_id]
+  gene_exons <- gene_exons[!is.na(exon), ]
+
   png(plotfile)
-  plot(totals$Start, totals$total, main = unique(counts$GeneID))
+  plot(totals$Start, totals$total, main = gene_id, ylim = c(-50, 1000))
+  for (i in seq_len(nrow(gene_exons))) {
+    lines(c(gene_exons$start[i], gene_exons$end[i]), c(-50, -50), col = "red", lwd = 5)
+  }
   dev.off()
 }
